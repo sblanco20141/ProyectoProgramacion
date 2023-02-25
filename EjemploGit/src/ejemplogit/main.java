ca01135848b4340bd8d8f06219d514a2fba75328
sblanco20141 <79016820+sblanco20141@users.noreply.github.com>
2023-02-25 10:42:10 -0600

    Cambio de main

--- EjemploGit/src/ejemplogit/main.java
+++ EjemploGit/src/ejemplogit/main.java
@@ -16,6 +16,7 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         //Esto es un cambio para que los estudiantes sepan como funciona
+        //Segundo cambio
     }
     
 }
